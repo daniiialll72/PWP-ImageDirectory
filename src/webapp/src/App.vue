6e201e8684d604dc6adf1224de1adddb72f2e0e5
SepehrSamadi <samadisepehr@gmail.com>
2023-03-06 17:22:10 +0200

    [1]: header component created. [2]: logo designed and customs fonts added

--- src/webapp/src/App.vue
+++ src/webapp/src/App.vue
@@ -1,24 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <Header/>
 </template>
 
 <script>
-
+import Header from './components/Main-Header'
 export default {
   name: 'App',
   components: {
-
+    Header
   }
 }
 </script>
 
 <style>
+
+body{
+  margin: 0;
+}
+html{
+  /*background-color: #35F29A;*/
+  -webkit-background-size: contain;
+  -moz-background-size: contain;
+  -o-background-size: contain;
+  background-size: 100% 100%;
+
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #5F5F5F;
 }
 </style>
