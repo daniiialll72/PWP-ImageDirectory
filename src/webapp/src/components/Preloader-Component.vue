f87d89680c3a9d6b446764dfcf473a44808f5fc3
SepehrSamadi <samadisepehr@gmail.com>
2023-03-07 14:51:00 +0200

    [1]: header component implemented

---
+++ src/webapp/src/components/Preloader-Component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pencil">
+    <div class="pencil__ball-point"></div>
+    <div class="pencil__cap"></div>
+    <div class="pencil__cap-base"></div>
+    <div class="pencil__middle"></div>
+    <div class="pencil__eraser"></div>
+  </div>
+  <div class="line"></div>
+</template>
+
+<script>
+import '../assets/css/styles.scss'
+export default {
+  name: "Preloader"
+}
+</script>
+
+<style scoped>
+
+</style>
